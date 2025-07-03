84337dbfa2ab325c28b70fe0ad0ac1dda43527d5
Aalexandram-l <aleksandracasina475@gmail.com>
2025-07-03 21:06:25 +0600

    Task08

---
+++ practice2025/CommandRunner/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Reflection;
+using CommandLib;
+
+namespace CommandRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dllPath = "FileSystemCommands.dll";
+
+            try
+            {
+                Assembly assembly = Assembly.LoadFrom(dllPath);
+                Type commandInterface = typeof(ICommand);
+
+                foreach (Type type in assembly.GetTypes())
+                {
+                    if (commandInterface.IsAssignableFrom(type) && !type.IsInterface)
+                    {
+                        if (Activator.CreateInstance(type) is ICommand command)
+                        {
+                            command.Execute();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+    }
+}
